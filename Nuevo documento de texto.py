7d36cf8bae3c9f301a7c3dbd4ca108342d4966b7
Larape84 <ljarape@misena.edu.co>
2022-06-15 13:51:02 -0500

    v2

--- Nuevo documento de texto.py
+++ Nuevo documento de texto.py
@@ -59,300 +59,300 @@ def abrir():
         robot.press("enter",presses=1)
         robot.press("tab",presses=1)
         robot.press("enter",presses=1)
-        
-    robot.moveTo(posicion)
-    robot.click(clicks=click)
-    robot.press("tab",presses=18)
-    robot.typewrite(str(nombres[1]))
-    time.sleep(1)
-    robot.press("tab",presses=1)
-    robot.typewrite(str(apellido1[1]))
-    time.sleep(1)
-    robot.press("tab",presses=1)
-    robot.typewrite(str(apellido2[1]))
-    time.sleep(1)
-    robot.press("tab",presses=1)
-    robot.press("enter",presses=1)
-
-    if (sexo[1]=="m"):
-        pya.hotkey('down')
-        robot.press("enter",presses=1)
-    if (sexo[1]=="f"):
-        pya.hotkey('down')
-        pya.hotkey('down')
-        robot.press("enter",presses=1)
-
-    def copiarFecha():
-        pyperclip.copy("") # <- This prevents last copy replacing current copy of null.
-        pyperclip.copy(fechanacimiento[1])
-        pya.hotkey('ctrl', 'c')
-        time.sleep(.01)  # ctrl-c is usually very fast but your program may execute faster
-        return pyperclip.paste()
-    
-    copiarFecha()
-    time.sleep(1)
-    robot.press("tab",presses=1)
-    keyboard.press(Key.ctrl)
-    keyboard.press('v')
-    keyboard.release('v')
-    keyboard.release(Key.ctrl)
-    robot.press("enter",presses=1)
-    robot.press("tab",presses=2)
-    robot.press("enter",presses=1)
-    time.sleep(1)
-    robot.press("tab",presses=1)
-    robot.press("enter",presses=1)   
-
-    time.sleep(5)
-
-    pya.hotkey('f6')
-    url2=copy_clipboard()
-
-    if("https://agenciapublicadeempleo.sena.edu.co/spe-web/spe/oferta/new"==url2):
-
+    else:
         robot.moveTo(posicion)
         robot.click(clicks=click)
-
-        robot.press("tab",presses=16)
-        robot.press("enter",presses=1)
+        robot.press("tab",presses=18)
+        robot.typewrite(str(nombres[1]))
+        time.sleep(1)
         robot.press("tab",presses=1)
-
-        pya.hotkey('space') #Colombia
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-
+        robot.typewrite(str(apellido1[1]))
+        time.sleep(1)
+        robot.press("tab",presses=1)
+        robot.typewrite(str(apellido2[1]))
+        time.sleep(1)
+        robot.press("tab",presses=1)
         robot.press("enter",presses=1)
 
-        time.sleep(2)
-        robot.press("tab",presses=1)
-        
-        pya.hotkey('space') #atlantico
-        pya.hotkey('down')
-        time.sleep(0.1)
-        pya.hotkey('down')
-        time.sleep(0.1)
-        pya.hotkey('down')
-        time.sleep(0.1)
-        pya.hotkey('down')
-        time.sleep(0.1)
-        robot.press("enter",presses=1)
-    
-        time.sleep(2)
-        robot.press("tab",presses=1)
+        if (sexo[1]=="m"):
+            pya.hotkey('down')
+            robot.press("enter",presses=1)
+        if (sexo[1]=="f"):
+            pya.hotkey('down')
+            pya.hotkey('down')
+            robot.press("enter",presses=1)
 
-        #barranquilla
-        pya.hotkey('space')
-        pya.hotkey('down')
-        time.sleep(0.1)
-        pya.hotkey('down')
-        time.sleep(0.1)
-        robot.press("enter",presses=1)
-        
-
-        robot.press("tab",presses=5)
-        robot.press("enter",presses=1)
-        robot.press("tab",presses=1)
-
-
-        pya.hotkey('space') #Colombia
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        robot.press("enter",presses=1)
-
-
-        time.sleep(2)
-        robot.press("tab",presses=1)
-        
-        pya.hotkey('space') #atlantico
-        pya.hotkey('down')
-        time.sleep(0.1)
-        pya.hotkey('down')
-        time.sleep(0.1)
-        pya.hotkey('down')
-        time.sleep(0.1)
-        pya.hotkey('down')
-        time.sleep(0.1)
-        robot.press("enter",presses=1)
-
-
-        time.sleep(2)
-        robot.press("tab",presses=1)
-
-        #barranquilla
-        pya.hotkey('space')
-        pya.hotkey('down')
-        time.sleep(0.1)
-        pya.hotkey('down')
-        time.sleep(0.1)
-        robot.press("enter",presses=1)
-
-        robot.scroll(4)
-
-        robot.press("tab",presses=1)
-        time.sleep(0.3)
-        robot.press("tab",presses=1)
-        time.sleep(0.3)
-        robot.press("tab",presses=1)
-        time.sleep(0.3)
-        robot.press("tab",presses=1)
-        time.sleep(0.3)
-        robot.press("tab",presses=1)
-        time.sleep(0.3)
-        robot.press("tab",presses=1)
-        time.sleep(0.3)
-        robot.press("tab",presses=1)
-        time.sleep(0.3)
-        robot.press("tab",presses=1)
-        time.sleep(0.3)
-        robot.press("tab",presses=1)
-        time.sleep(0.3)
-        robot.press("tab",presses=1)
-        time.sleep(0.3)
-
-        robot.press("enter",presses=1)
-        pya.hotkey('down') #estadocivil
-        robot.press("enter",presses=1)
-
-        def copiarPersonas():
-            pyperclip.copy("") 
-            pyperclip.copy("2") # personas a cargo
+        def copiarFecha():
+            pyperclip.copy("") # <- This prevents last copy replacing current copy of null.
+            pyperclip.copy(fechanacimiento[1])
             pya.hotkey('ctrl', 'c')
             time.sleep(.01)  # ctrl-c is usually very fast but your program may execute faster
             return pyperclip.paste()
-            
-        copiarPersonas()
+        
+        copiarFecha()
         time.sleep(1)
         robot.press("tab",presses=1)
         keyboard.press(Key.ctrl)
         keyboard.press('v')
         keyboard.release('v')
-        keyboard.release(Key.ctrl) #personas a cargo
-
-        robot.press("tab",presses=1)
-
-        pya.hotkey('down') #posicion familiar
-
-        robot.press("tab",presses=1)
-
-        pya.hotkey('down') #libreta
-        robot.press("tab",presses=1)
-        pya.hotkey('down') #no migrante
-        robot.press("tab",presses=1)
-        pya.hotkey('down') #urbana
-        robot.press("tab",presses=1)
-        time.sleep(0.3)
-        robot.press("tab",presses=1)
-        time.sleep(0.3)
+        keyboard.release(Key.ctrl)
         robot.press("enter",presses=1)
-        pya.hotkey('down') #salario
-        time.sleep(0.3)
-        pya.hotkey('down') #salario
-        time.sleep(0.3)
-        pya.hotkey('down') #salario
-        time.sleep(0.3)
-        robot.press("enter",presses=1)
-        robot.press("tab",presses=1)
-
-        pya.hotkey('space') #Colombia
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        pya.hotkey('c',presses=1)
-        time.sleep(0.1)
-        robot.press("enter",presses=1)
-        time.sleep(2)
-
-        robot.press("tab",presses=1)
-        pya.hotkey('space') #atlantico
-        pya.hotkey('down')
-        time.sleep(0.1)
-        pya.hotkey('down')
-        time.sleep(0.1)
-        pya.hotkey('down')
-        time.sleep(0.1)
-        pya.hotkey('down')
-        time.sleep(0.1)
-        robot.press("enter",presses=1)
-        time.sleep(2)
-
-        robot.press("tab",presses=1)
-        #barranquilla
-        pya.hotkey('space')
-        pya.hotkey('down')
-        time.sleep(0.1)
-        pya.hotkey('down')
-        time.sleep(0.1)
-        robot.press("enter",presses=1)
-
-        robot.press("tab",presses=3)
-        robot.typewrite("BARRANQUILLA")# DIRECCION
-        time.sleep(0.3)
-
-        robot.press("tab",presses=3)
-        pya.hotkey('down')
-
-        robot.press("tab",presses=1)
-        robot.typewrite("3155497360")# TELEFONO
-        time.sleep(0.3)
-        robot.press("tab",presses=8)
-        robot.typewrite("LJARAPEMISENA.EDU.CO")# CORREO
-
-        robot.press("tab",presses=1)
-        robot.typewrite("BACHILLER")# PROFESION
-        time.sleep(1)
         robot.press("tab",presses=2)
         robot.press("enter",presses=1)
+        time.sleep(1)
+        robot.press("tab",presses=1)
+        robot.press("enter",presses=1)   
+
+        time.sleep(5)
+
+        pya.hotkey('f6')
+        url2=copy_clipboard()
+
+        if("https://agenciapublicadeempleo.sena.edu.co/spe-web/spe/oferta/new"==url2):
+
+            robot.moveTo(posicion)
+            robot.click(clicks=click)
+
+            robot.press("tab",presses=16)
+            robot.press("enter",presses=1)
+            robot.press("tab",presses=1)
+
+            pya.hotkey('space') #Colombia
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+
+            robot.press("enter",presses=1)
+
+            time.sleep(2)
+            robot.press("tab",presses=1)
+            
+            pya.hotkey('space') #atlantico
+            pya.hotkey('down')
+            time.sleep(0.1)
+            pya.hotkey('down')
+            time.sleep(0.1)
+            pya.hotkey('down')
+            time.sleep(0.1)
+            pya.hotkey('down')
+            time.sleep(0.1)
+            robot.press("enter",presses=1)
+        
+            time.sleep(2)
+            robot.press("tab",presses=1)
+
+            #barranquilla
+            pya.hotkey('space')
+            pya.hotkey('down')
+            time.sleep(0.1)
+            pya.hotkey('down')
+            time.sleep(0.1)
+            robot.press("enter",presses=1)
+
+
+            robot.press("tab",presses=5)
+            robot.press("enter",presses=1)
+            robot.press("tab",presses=1)
+
+
+            pya.hotkey('space') #Colombia
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            robot.press("enter",presses=1)
+
+
+            time.sleep(2)
+            robot.press("tab",presses=1)
+            
+            pya.hotkey('space') #atlantico
+            pya.hotkey('down')
+            time.sleep(0.1)
+            pya.hotkey('down')
+            time.sleep(0.1)
+            pya.hotkey('down')
+            time.sleep(0.1)
+            pya.hotkey('down')
+            time.sleep(0.1)
+            robot.press("enter",presses=1)
+
+
+            time.sleep(2)
+            robot.press("tab",presses=1)
+
+            #barranquilla
+            pya.hotkey('space')
+            pya.hotkey('down')
+            time.sleep(0.1)
+            pya.hotkey('down')
+            time.sleep(0.1)
+            robot.press("enter",presses=1)
+
+            robot.scroll(4)
+
+            robot.press("tab",presses=1)
+            time.sleep(0.3)
+            robot.press("tab",presses=1)
+            time.sleep(0.3)
+            robot.press("tab",presses=1)
+            time.sleep(0.3)
+            robot.press("tab",presses=1)
+            time.sleep(0.3)
+            robot.press("tab",presses=1)
+            time.sleep(0.3)
+            robot.press("tab",presses=1)
+            time.sleep(0.3)
+            robot.press("tab",presses=1)
+            time.sleep(0.3)
+            robot.press("tab",presses=1)
+            time.sleep(0.3)
+            robot.press("tab",presses=1)
+            time.sleep(0.3)
+            robot.press("tab",presses=1)
+            time.sleep(0.3)
+
+            robot.press("enter",presses=1)
+            pya.hotkey('down') #estadocivil
+            robot.press("enter",presses=1)
+
+            def copiarPersonas():
+                pyperclip.copy("") 
+                pyperclip.copy("2") # personas a cargo
+                pya.hotkey('ctrl', 'c')
+                time.sleep(.01)  # ctrl-c is usually very fast but your program may execute faster
+                return pyperclip.paste()
+                
+            copiarPersonas()
+            time.sleep(1)
+            robot.press("tab",presses=1)
+            keyboard.press(Key.ctrl)
+            keyboard.press('v')
+            keyboard.release('v')
+            keyboard.release(Key.ctrl) #personas a cargo
+
+            robot.press("tab",presses=1)
+
+            pya.hotkey('down') #posicion familiar
+
+            robot.press("tab",presses=1)
+
+            pya.hotkey('down') #libreta
+            robot.press("tab",presses=1)
+            pya.hotkey('down') #no migrante
+            robot.press("tab",presses=1)
+            pya.hotkey('down') #urbana
+            robot.press("tab",presses=1)
+            time.sleep(0.3)
+            robot.press("tab",presses=1)
+            time.sleep(0.3)
+            robot.press("enter",presses=1)
+            pya.hotkey('down') #salario
+            time.sleep(0.3)
+            pya.hotkey('down') #salario
+            time.sleep(0.3)
+            pya.hotkey('down') #salario
+            time.sleep(0.3)
+            robot.press("enter",presses=1)
+            robot.press("tab",presses=1)
+
+            pya.hotkey('space') #Colombia
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            pya.hotkey('c',presses=1)
+            time.sleep(0.1)
+            robot.press("enter",presses=1)
+            time.sleep(2)
+
+            robot.press("tab",presses=1)
+            pya.hotkey('space') #atlantico
+            pya.hotkey('down')
+            time.sleep(0.1)
+            pya.hotkey('down')
+            time.sleep(0.1)
+            pya.hotkey('down')
+            time.sleep(0.1)
+            pya.hotkey('down')
+            time.sleep(0.1)
+            robot.press("enter",presses=1)
+            time.sleep(2)
+
+            robot.press("tab",presses=1)
+            #barranquilla
+            pya.hotkey('space')
+            pya.hotkey('down')
+            time.sleep(0.1)
+            pya.hotkey('down')
+            time.sleep(0.1)
+            robot.press("enter",presses=1)
+
+            robot.press("tab",presses=3)
+            robot.typewrite("BARRANQUILLA")# DIRECCION
+            time.sleep(0.3)
+
+            robot.press("tab",presses=3)
+            pya.hotkey('down')
+
+            robot.press("tab",presses=1)
+            robot.typewrite("3155497360")# TELEFONO
+            time.sleep(0.3)
+            robot.press("tab",presses=8)
+            robot.typewrite("LJARAPEMISENA.EDU.CO")# CORREO
+
+            robot.press("tab",presses=1)
+            robot.typewrite("BACHILLER")# PROFESION
+            time.sleep(1)
+            robot.press("tab",presses=2)
+            robot.press("enter",presses=1)
 
 
 
